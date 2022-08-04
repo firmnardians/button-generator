2c0bae9f7a4071eff0ac88bcbddd15e8043c6e1d
hesoyam <user@users-MacBook-Pro.local>
2022-08-04 13:30:00 +0700

    memo

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,7 +1,9 @@
-export default function Card({ children }) {
+import { memo } from 'react';
+
+export default memo(function Card({ children }) {
 	return (
 		<div className='card shadow-lg'>
 			<div className='card-body'>{children}</div>
 		</div>
 	);
-}
+});
